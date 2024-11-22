200a0df1fb068e8552a87a0c1f3ad5df056fcb5f
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2024-11-22 20:09:26 +0300

    feat: add return button

--- src/pages/tasks.tsx
+++ src/pages/tasks.tsx
@@ -7,39 +7,43 @@ import {
 } from "@models/courses";
 import { wasona } from "@utils/wasona";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 export const Tasks: React.FC = () => {
-  const [tasks, setTasks] = useState<string[]>();
+  const [done, setDone] = useState(false);
+  const [taskQueue, setTaskQueue] = useState<string[]>();
   const [question, setQuestion] = useState<QuestionRequestT>();
   const { id: lessonId } = useParams();
+  const navigate = useNavigate();
 
+  // When lessonId is updated, load task queue
   useEffect(() => {
     (async () => {
       const response = await wasona<TasksRequestT>("get", "/app/tasks", {
         params: { lessonId: lessonId },
       });
-      if (response) setTasks(response.data!.tasks);
+      if (response) setTaskQueue(response.data!.tasks);
     })();
   }, [lessonId]);
 
+  // When task queue is updated, load 0th element as
   useEffect(() => {
-    if (!tasks || !tasks.length) return;
+    if (!taskQueue) return;
+    if (taskQueue.length == 0) {
+      setDone(true);
+      return;
+    }
     (async () => {
       const response = await wasona<QuestionRequestT>(
         "get",
         "/app/task-question",
         {
-          params: { taskId: tasks[0] },
+          params: { taskId: taskQueue[0] },
         },
       );
       if (response) setQuestion(response);
     })();
-  }, [tasks]);
-
-  useEffect(() => {
-    // alert(`Received a question: ${JSON.stringify(question)}`);
-  }, [question]);
+  }, [taskQueue]);
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key == "Enter") handleSubmit((e.target as HTMLInputElement).value);
@@ -48,28 +52,33 @@ export const Tasks: React.FC = () => {
   const handleSubmit = (text: string) => {
     (async () => {
       const response = await wasona<AnswerRequestT>("get", "/app/task-answer", {
-        params: { taskId: tasks![0], reply: text },
+        params: { taskId: taskQueue![0], reply: text },
       });
       if (response && response.success) {
         alert(response.data!.correct);
-        setTasks(tasks!.splice(1));
+        setTaskQueue(taskQueue!.slice(1));
+        console.log(taskQueue);
       }
     })();
   };
 
+  const Loading = <p>Loading the next question, please wait...</p>;
+  const Done = (
+    <>
+      <p>You're all done! Good job!</p>
+      <button onClick={() => navigate(-1)}>Back</button>
+    </>
+  );
+  const Question = (
+    <>
+      <p>{question?.data?.task.give}</p>
+      <input onKeyDown={handleKeyDown}></input>
+    </>
+  );
   return (
     <>
       <Navbar />
-      <WidthLimiter>
-        {question?.success ? (
-          <>
-            <p>{question?.data?.task.give}</p>
-            <input onKeyDown={handleKeyDown}></input>
-          </>
-        ) : (
-          <p>{JSON.stringify(question?.error)}</p>
-        )}
-      </WidthLimiter>
+      <WidthLimiter>{done ? Done : question ? Question : Loading}</WidthLimiter>
     </>
   );
 };
